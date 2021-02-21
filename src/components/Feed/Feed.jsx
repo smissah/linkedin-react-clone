fd9be024eb164a952d724ad2fa5765026c45bf48
stephenPC <stephenmissah@outlook.com>
2021-02-21 19:44:00 +0000

    useState on submit not going to DB

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../componentStyles/feedStyles/feedStyle.css";
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
@@ -7,21 +7,55 @@ import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import InputOption from "../InputOption";
 import Post from "../Posts/Post";
+import { db } from "../../firebase";
+import firebase from "firebase";
 
 const Feed = () => {
+  const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // realtime listener for database
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            data: doc.data(),
+          };
+        })
+      )
+    );
+  }, []);
+  //functions
 
   const handlePostSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    console.log("sumit");
+    //push to database
+    db.collection("posts").add({
+      name: "Stephen Missah",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      //using server timestamp keeps time the same regardless of lcation
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
   };
+
+  //----------------
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
-          <form onSubmit={handlePostSubmit}>
-            <input type="text" placeholder="Create an awesome post" />
+          <form onSubmit={() => handlePostSubmit}>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              placeholder="Create an awesome post"
+            />
             <button type="submit"></button>
           </form>
         </div>
@@ -39,10 +73,15 @@ const Feed = () => {
       </div>
 
       <div className="feed__posts">
-        {posts.map((post, index) => {
-          <Post key={index} name="" description="" message="" imageUrl="" />;
+        {posts.map(({ id, data: { name, description, message, photoUrl } }) => {
+          <Post
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />;
         })}
-        <Post name="stephen missah" message="My first post on Linkedin" />;
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBSjqZgu59xl0hKHwJKpr5r6PIRxduP4hs",
   authDomain: "linkedin-react-clone-b1afd.firebaseapp.com",
   projectId: "linkedin-react-clone-b1afd",
   storageBucket: "linkedin-react-clone-b1afd.appspot.com",
   messagingSenderId: "688383384717",
-  appId: "1:688383384717:web:d353a5013dfcb3bf8e96c4"
+  appId: "1:688383384717:web:d353a5013dfcb3bf8e96c4",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
