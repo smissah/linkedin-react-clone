d858d906e0981370e9b1e4aceda2f54ea32c4133
stephenPC <stephenmissah@outlook.com>
2021-02-21 22:58:25 +0000

    ts45 redux time!

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -6,18 +6,15 @@ export const userSlice = createSlice({
     user: null,
   },
   reducers: {
-    increment: (state) => {
+    login: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
+      state.value = action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    logout: (state) => {
+      state.user = null;
     },
   },
 });
