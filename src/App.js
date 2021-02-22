863224622811e4cc28a446a681958e61a5335dc5
Stephen M <stephenmissah@outlook.com>
2021-02-22 17:16:29 +0000

    login page created, needs styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from "react";
 import "./App.css";
-
+import { selectUser } from "./features/userSlice";
 import Feed from "./components/Feed/Feed";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Widget from "./components/Widget/Widget";
+import Login from "./components/Login/Login";
 
 //components
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
+  const user = userSelector(selectUser);
+
+  <div className="app">
+    <Header />
+
+    {!user ? (
+      <Login />
+    ) : (
       <div className="app__body container">
+        {" "}
         <Sidebar />
         <Feed />
         <Widget />
       </div>
-    </div>
-  );
+    )}
+  </div>;
 }
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../componentStyles/loginStyles";
+
+const Login = () => {
+  return <div className="login"> log in</div>;
+};
+
+export default Login;
