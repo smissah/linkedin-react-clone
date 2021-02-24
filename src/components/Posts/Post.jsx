6e21f378f1549181f141cdd9d83a452fd0124ec8
stephenPC <stephenmissah@outlook.com>
2021-02-24 19:07:04 +0000

    added react forward ref for animation: yarn add react-flip-move

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,5 +1,5 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { forwardRef } from "react";
 import "../componentStyles/postsStyles/post.css";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ShareIcon from "@material-ui/icons/Share";
@@ -7,33 +7,38 @@ import SendIcon from "@material-ui/icons/Send";
 import ChatIcon from "@material-ui/icons/Chat";
 import InputOption from "../InputOption";
 
-const Post = ({
-  name = "unknown",
-  message = "no message",
-  description = "no description",
-  photoUrl = "",
-}) => {
-  return (
-    <div className="post">
-      <div className="post__header">
-        <Avatar src={photoUrl}>{name?.[0].toLocaleUpperCase()}</Avatar>
-        <div className="post__info">
-          <h2>{name}</h2>
-          <p>{description}</p>
+const Post = forwardRef(
+  (
+    {
+      name = "unknown",
+      message = "no message",
+      description = "no description",
+      photoUrl = "",
+    },
+    ref
+  ) => {
+    return (
+      <div className="post">
+        <div className="post__header">
+          <Avatar src={photoUrl}>{name?.[0].toLocaleUpperCase()}</Avatar>
+          <div className="post__info">
+            <h2>{name}</h2>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
-      <div className="post__body">
-        <p className="post__message">{message}</p>
+        <div className="post__body">
+          <p className="post__message">{message}</p>
 
-        <div className="post__buttons">
-          <InputOption Icon={ThumbUpAltIcon} title="Like" color="grey" />
-          <InputOption Icon={ChatIcon} title="Comment" color="grey" />
-          <InputOption Icon={ShareIcon} title="Share" color="grey" />
-          <InputOption Icon={SendIcon} title="Send" color="grey" />
+          <div className="post__buttons">
+            <InputOption Icon={ThumbUpAltIcon} title="Like" color="grey" />
+            <InputOption Icon={ChatIcon} title="Comment" color="grey" />
+            <InputOption Icon={ShareIcon} title="Share" color="grey" />
+            <InputOption Icon={SendIcon} title="Send" color="grey" />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Post;
