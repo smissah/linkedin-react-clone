715f44aa9571925ba5f1d27e10ef619919a0ad64
stephenPC <stephenmissah@outlook.com>
2021-02-24 19:34:45 +0000

    issues with widget rendering

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,8 +1,29 @@
+import { Info } from "@material-ui/icons";
 import React from "react";
 import "../componentStyles/widgetStyles/Widget.css";
-
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 const Widget = () => {
-  return <div className="widget container">i am widgets</div>;
+  const newsLetters = (heading, subtitle) => {
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widets__articleRight">
+        <h2>{heading}</h2>
+        <p>{subtitle}</p>
+      </div>
+    </div>;
+  };
+  return (
+    <div className="widget container">
+      <div className="widget__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsLetters("Is Redux dead?", "Find out why - 500 readers")}
+    </div>
+  );
 };
 
 export default Widget;
