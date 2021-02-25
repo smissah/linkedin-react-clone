d5fdc5561403bb90cd5228fc5ad5eb35db6a61a7
stephenPC <stephenmissah@outlook.com>
2021-02-25 11:20:56 +0000

    removed redundent useDispatch from sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../componentStyles/sidebarStyles/sidebar.css";
 import { Avatar } from "@material-ui/core";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
 const Sidebar = () => {
   const recentItems = (topic) => {
